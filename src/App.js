476a737ae9ec0ab2fd43c2f88d1ec2fa70e0ec47
Leong Jin Kai <leongjinkai@gmail.com>
2024-10-16 23:54:08 +0800

    change url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
 import { useState, useRef } from "react";
+import { CSVLink } from "react-csv";
+
+const headers = [
+	{ label: "First Name", key: "firstname" },
+	{ label: "Last Name", key: "lastname" },
+	{ label: "Email", key: "email" },
+];
+
+const data = [
+	{ firstname: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
+	{ firstname: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
+	{ firstname: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" },
+];
 
 function App() {
 	const [file, setFile] = useState(null);
-  const hiddenFileInput = useRef(null);
+  const [csvFile, setCsvFile] = useState(null);
+	const hiddenFileInput = useRef(null);
 
 	const handleFileChange = (e) => {
 		if (e.target.files) {
@@ -21,12 +35,14 @@ function App() {
 
 			try {
 				// You can write the URL of your server or any other endpoint used for file upload
-				const result = await fetch("https://httpbin.org/post", {
+				const result = await fetch("https://slip-reader-processor-415f9c26f2e3.herokuapp.com/upload", {
 					method: "POST",
 					body: formData,
 				});
 
 				const data = await result.json();
+
+        setCsvFile(data)
 
 				console.log(data);
 			} catch (error) {
@@ -35,24 +51,27 @@ function App() {
 		}
 	};
 
-  const handleClick = () => {
-    hiddenFileInput.current.click();
-  };
+	const handleClick = () => {
+		hiddenFileInput.current.click();
+	};
 
 	return (
 		<div className="flex justify-center items-center h-[100vh]">
 			<header className="flex flex-col gap-10 items-center">
-        <p className="text-4xl font-bold">Welcome</p>
+				<p className="text-4xl font-bold">Welcome</p>
 				<p className="text-2xl">Upload PDF below.</p>
 				<div className="flex flex-col">
-					<button className="bg-gray-500 text-white px-10 py-5 rounded-md text-2xl" onClick={handleClick}>
+					<button
+						className="bg-gray-500 text-white px-10 py-5 rounded-md text-2xl"
+						onClick={handleClick}
+					>
 						Choose a file
 					</button>
 					<input
 						id="file"
 						type="file"
 						className="invisible"
-            ref={hiddenFileInput}
+						ref={hiddenFileInput}
 						onChange={handleFileChange}
 					/>
 				</div>
@@ -74,6 +93,9 @@ function App() {
 						Upload pdf
 					</button>
 				)}
+				{csvFile && <CSVLink data={data} headers={headers} className="bg-teal-500 text-white px-10 py-5 rounded-md">
+					Download csv here
+				</CSVLink>}
 			</header>
 		</div>
 	);
